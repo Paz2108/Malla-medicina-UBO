a08d562cde96148d0436fbba9f364cbec99f6656
Paz2108 <pasita.rodriguez.pinares@gmail.com>
2025-07-26 23:56:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.querySelectorAll('.ramo').forEach(ramo => {
-  ramo.addEventListener('click', () => {
-    const siguienteId = ramo.dataset.siguiente;
-    if (siguienteId) {
-      const siguienteRamo = document.getElementById(siguienteId);
-      if (siguienteRamo.classList.contains('oculto')) {
-        siguienteRamo.classList.remove('oculto');
-      }
-    }
-  });
-});
+
